998ffdcd3eb8cbea37e9c5a332629f7c92e57bd8
ravishankar559 <kravishankarreddy96@gmail.com>
2020-10-13 23:20:31 +0530

    LDAP Login Form Fully Authenticated

---
+++ src/main/java/com/sample/SpringRestServices/Controller/HomeResource.java
@@ -0,0 +1,13 @@
+package com.sample.SpringRestServices.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeResource {
+	
+	@GetMapping("/")
+	public String index() {
+		return "Sample LDAP";
+	}
+}
